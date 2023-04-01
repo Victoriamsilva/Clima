2a4f98dce082bec86f5e99af6925581eed24e287
Victoriamsilva <victoria.marques8424@gmail.com>
2023-03-31 21:17:21 -0300

    feat: create route and config eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Routes from "./Routes/router"
+import { Container } from "./Styles/globalStyles"
+
+function App() {
+  return (
+    <Container>
+      <Routes />
+    </Container>
+  )
+}
+
+export default App
